3bbb43b72520085adb393a7754b86b21b5525836
Terence <wrze@sina.com>
2017-11-21 16:22:43 +0800

    clean code

--- src/components/checkList.test.js
+++ src/components/checkList.test.js
@@ -79,7 +79,6 @@ export default class CheckListTest extends React.Component {
 
     componentWillMount() {
         this.prepareCheckListData();
-        alert(this.listArr);
     }
 
     render() {
